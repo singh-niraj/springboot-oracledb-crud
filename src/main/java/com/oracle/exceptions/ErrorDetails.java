6068dddef438b9a764937cbf9718ced07653a0d1
Niraj Singh <>
2025-08-22 10:34:39 +0530

    code-commit

--- src/main/java/com/oracle/exceptions/ErrorDetails.java
+++ src/main/java/com/oracle/exceptions/ErrorDetails.java
@@ -1,51 +1,32 @@
 package com.oracle.exceptions;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Getter
+@Setter
 public class ErrorDetails {
 
-	private Date timestamp;
-	private String message;
-	private String details;
+    private Date timestamp;
+    private String message;
+    private String details;
 
-	public ErrorDetails() {
-		super();
-	}
+    public ErrorDetails() {
+        super();
+    }
 
-	public ErrorDetails(Date timestamp, String message, String details) {
-		super();
-		this.timestamp = timestamp;
-		this.message = message;
-		this.details = details;
-	}
+    public ErrorDetails(Date timestamp, String message, String details) {
+        super();
+        this.timestamp = timestamp;
+        this.message = message;
+        this.details = details;
+    }
 
-	public Date getTimestamp() {
-		return timestamp;
-	}
-
-	public void setTimestamp(Date timestamp) {
-		this.timestamp = timestamp;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
-
-	public String getDetails() {
-		return details;
-	}
-
-	public void setDetails(String details) {
-		this.details = details;
-	}
-
-	@Override
-	public String toString() {
-		return "ErrorDetails [timestamp=" + timestamp + ", message=" + message + ", details=" + details + "]";
-	}
+    @Override
+    public String toString() {
+        return "ErrorDetails [timestamp=" + timestamp + ", message=" + message + ", details=" + details + "]";
+    }
 
 }
--- src/main/java/com/oracle/repository/EmployeeRepository.java
+++ src/main/java/com/oracle/repository/EmployeeRepository.java
@@ -1,11 +1,10 @@
 package com.oracle.repository;
 
+import com.oracle.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.oracle.entity.Employee;
-
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
